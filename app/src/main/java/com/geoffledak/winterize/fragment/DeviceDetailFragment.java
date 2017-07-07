6ab87ff842ac88e92272e98bab3f4f90d9d68a95
Geoff Ledak <geoffledak@gmail.com>
2017-07-06 18:20:09 -0600

    Saving progress

--- app/src/main/java/com/geoffledak/winterize/fragment/DeviceDetailFragment.java
+++ app/src/main/java/com/geoffledak/winterize/fragment/DeviceDetailFragment.java
@@ -8,13 +8,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.geoffledak.winterize.R;
 import com.geoffledak.winterize.activity.MainActivity;
 import com.geoffledak.winterize.model.Device;
+import com.geoffledak.winterize.model.Info;
+import com.geoffledak.winterize.service.RachioClient;
+import com.geoffledak.winterize.utils.APIUtils;
 import com.geoffledak.winterize.utils.AppKeys;
 
 import org.parceler.Parcels;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 /**
  * Created by Turbo9000 on 7/6/2017.
@@ -23,6 +32,7 @@ import org.parceler.Parcels;
 public class DeviceDetailFragment extends Fragment {
 
     View mView;
+    MainActivity mActivity;
     TextView mModel;
     Device mDevice;
     Switch mStandbyModeSwitch;
@@ -32,6 +42,7 @@ public class DeviceDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         mView = inflater.inflate(R.layout.fragment_device_detail, container, false);
+        mActivity = (MainActivity) getContext();
         mDevice = Parcels.unwrap(getArguments().getParcelable(AppKeys.KEY_SELECTED_DEVICE));
 
         mModel = (TextView) mView.findViewById(R.id.model);
@@ -55,9 +66,71 @@ public class DeviceDetailFragment extends Fragment {
 
     private void handleStandbyModeSwitchClick() {
 
+        if( mDevice.isOn() ) {
 
+            // use Retrofit to turn device off (on = false);
+            turnDeviceOff();
+        }
+        else {
+            // use Retrofit to turn device on (on = true);
+            turnDeviceOn();
+        }
 
     }
+
+
+
+
+    private void turnDeviceOn() {
+
+        Retrofit retrofit = APIUtils.buildRetrofit(getContext());
+        RachioClient client = retrofit.create(RachioClient.class);
+
+        Call<String> call = client.turnDeviceOn(mActivity.getAPIToken(), new Info(mDevice.getId()));
+
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+
+                Toast.makeText(getContext(), "Device successfully turned on", Toast.LENGTH_SHORT).show();
+                mDevice.setOn(true);
+                // loadInfoFull();
+
+            }
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+    private void turnDeviceOff() {
+
+        Retrofit retrofit = APIUtils.buildRetrofit(getContext());
+        RachioClient client = retrofit.create(RachioClient.class);
+
+        Call<String> call = client.turnDeviceOff(mActivity.getAPIToken(), new Info(mDevice.getId()));
+
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+
+                Toast.makeText(getContext(), "Device successfully turned off", Toast.LENGTH_SHORT).show();
+                mDevice.setOn(false);
+                // loadInfoFull();
+
+            }
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
 
 
 }
